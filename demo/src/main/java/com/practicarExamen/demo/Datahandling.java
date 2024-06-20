f45962231885bbb0167e135b3a2db3c0c05d9094
javiere19 <94833548+javiere19@users.noreply.github.com>
2024-06-20 10:15:07 +0200

    3º commit back

--- demo/src/main/java/com/practicarExamen/demo/Datahandling.java
+++ demo/src/main/java/com/practicarExamen/demo/Datahandling.java
@@ -1,4 +1,38 @@
 package com.practicarExamen.demo;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.lang.reflect.Type;
 public class Datahandling {
+    public static void saveFile (ArrayList<Spaceship> spaceshipList) {
+        Gson gson = new Gson();
+
+        try {
+            FileWriter writer = new FileWriter("ships.json");
+            gson.toJson(spaceshipList, writer);
+            writer.flush();
+            writer.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static ArrayList<Spaceship> getShips () {
+        Gson gson = new Gson();
+        Type spaceshipsListType = new TypeToken<ArrayList<Spaceship>>(){}.getType();
+        try {
+            return gson.fromJson(new FileReader("ships.json"), spaceshipsListType);
+        } catch (FileNotFoundException e) {
+            System.out.println("Fichero no encontrado. Se devuelve un array vacío");
+            return new ArrayList<>();
+        }
+    }
 }
